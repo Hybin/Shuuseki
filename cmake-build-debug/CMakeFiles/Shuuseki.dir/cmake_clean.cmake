2a714f3873e11306f460cc922a1de9941d900d8a
Hybin <hhb8550@live.com>
2018-06-08 10:53:13 +0800

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Shuuseki.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shuuseki.dir/main.cpp.o"
+  "Shuuseki.pdb"
+  "Shuuseki"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Shuuseki.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
